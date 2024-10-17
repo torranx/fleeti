9bee4de7a189b69326aae23533c47b1c14cb9d15
torranx <torranicles@gmail.com>
2024-10-17 08:23:23 +0800

    landing page updates

---
+++ frontend/src/components/custom/header.tsx
@@ -0,0 +1,29 @@
+export default function Header() {
+  return (
+    <header className="absolute inset-x-0 top-0 z-50">
+      <nav aria-label="Global" className="flex items-center justify-between p-6 lg:px-8">
+        <div className="flex lg:flex-1">
+          <a href="#" className="-m-1.5 p-1.5">
+            <span className="sr-only">Your Company</span>
+            <img
+              alt=""
+              src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=600"
+              className="h-8 w-auto"
+            />
+          </a>
+        </div>
+        <div className="flex flex-1 items-center justify-end gap-x-6">
+          <a href="#" className="hidden lg:block lg:text-sm lg:font-semibold lg:leading-6 lg:text-gray-900">
+            Log in
+          </a>
+          <a
+            href="#"
+            className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >
+            Sign up
+          </a>
+        </div>
+      </nav>
+    </header>
+  )
+}
