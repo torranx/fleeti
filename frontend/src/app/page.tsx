5466560759d5c2116963d0f103d1f896eeb989f9
torranx <torranicles@gmail.com>
2024-10-21 02:51:07 +0800

    fix jsx linting

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -93,17 +93,17 @@ export default function LandingPage() {
           </div>
           <div className="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl">
             <dl className="grid max-w-xl grid-cols-1 gap-x-8 gap-y-10 lg:max-w-none lg:grid-cols-2 lg:gap-y-16">
-              {features.map((feature) => (
+              { features.map((feature) => (
                 <div key={ feature.name } className="relative pl-16">
                   <dt className="text-base font-semibold leading-7 text-gray-900">
                     <div className="absolute left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-indigo-600">
                       <feature.icon aria-hidden="true" className="h-6 w-6 text-white" />
                     </div>
-                    {feature.name}
+                    { feature.name }
                   </dt>
-                  <dd className="mt-2 text-base leading-7 text-gray-600">{feature.description}</dd>
+                  <dd className="mt-2 text-base leading-7 text-gray-600">{ feature.description }</dd>
                 </div>
-              ))}
+              )) }
             </dl>
           </div>
         </div>
