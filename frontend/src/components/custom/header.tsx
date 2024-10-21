a003a7c1e4d1fd03e6d0358348ce11558cdae389
torranx <torranicles@gmail.com>
2024-10-22 02:15:54 +0800

    phaseout headlessui/react

--- frontend/src/components/custom/header.tsx
+++ frontend/src/components/custom/header.tsx
@@ -1,11 +1,12 @@
 "use client"
 
 import { useState } from "react"
-import { Dialog, DialogPanel } from "@headlessui/react"
-import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline"
+import { Bars3Icon } from "@heroicons/react/24/outline"
 import Link from "next/link"
 import Image from "next/image"
 import { Button } from "../ui/button"
+import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle, SheetTrigger } from "../ui/sheet"
+import { VisuallyHidden } from "@radix-ui/react-visually-hidden"
 
 const navigation = [
   { name: "Features", href: "/#features-section" },
@@ -32,14 +33,60 @@ export default function Header() {
           </Link>
         </div>
         <div className="flex lg:hidden">
-          <button
-            type="button"
-            onClick={ () => setMobileMenuOpen(true) }
-            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
-          >
-            <span className="sr-only">Open main menu</span>
-            <Bars3Icon aria-hidden="true" className="h-6 w-6" />
-          </button>
+          <Sheet open={ mobileMenuOpen } onOpenChange={ setMobileMenuOpen }>
+            <SheetTrigger>
+              <span className="sr-only">Open main menu</span>
+              <Bars3Icon aria-hidden="true" className="h-6 w-6" />
+            </SheetTrigger>
+            <SheetContent className="w-[310px]">
+              <SheetHeader>
+                <VisuallyHidden asChild>
+                  <SheetTitle>Menu - Mobile</SheetTitle>
+                </VisuallyHidden>
+                <VisuallyHidden asChild>
+                  <SheetDescription>
+                    Menu items for mobile
+                  </SheetDescription>
+                </VisuallyHidden>
+              </SheetHeader>
+              <div className="flex items-center justify-between">
+                <Link href="/" className="-m-2">
+                  <span className="sr-only">Your Company</span>
+                  <Image
+                    alt=""
+                    src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=600"
+                    className="h-8 w-auto"
+                    width={ 12 }
+                    height={ 12 }
+                  />
+                </Link>
+              </div>
+              <div className="mt-6 flow-root">
+                <div className="-my-6 divide-y divide-gray-500/10">
+                  <div className="space-y-2 py-6">
+                    { navigation.map((item) => (
+                      <Link
+                        onClick={ () => setMobileMenuOpen(false) }
+                        key={ item.name }
+                        href={ item.href }
+                        className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                      >
+                        { item.name }
+                      </Link>
+                    )) }
+                  </div>
+                  <div className="py-6">
+                    <Link
+                      href="/login"
+                      className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                    >
+                      Log in
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </SheetContent>
+          </Sheet>
         </div>
         <div className="hidden lg:flex lg:gap-x-12">
           { navigation.map((item) => (
@@ -56,54 +103,6 @@ export default function Header() {
           </Link>
         </div>
       </nav>
-      <Dialog open={ mobileMenuOpen } onClose={ setMobileMenuOpen } className="lg:hidden">
-        <div className="fixed inset-0 z-50" />
-        <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
-          <div className="flex items-center justify-between">
-            <Link href="/" className="-m-1.5 p-1.5">
-              <span className="sr-only">Your Company</span>
-              <Image
-                alt=""
-                src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=600"
-                className="h-8 w-auto"
-                width={ 32 }
-                height={ 32 }
-              />
-            </Link>
-            <button
-              type="button"
-              onClick={ () => setMobileMenuOpen(false) }
-              className="-m-2.5 rounded-md p-2.5 text-gray-700"
-            >
-              <span className="sr-only">Close menu</span>
-              <XMarkIcon aria-hidden="true" className="h-6 w-6" />
-            </button>
-          </div>
-          <div className="mt-6 flow-root">
-            <div className="-my-6 divide-y divide-gray-500/10">
-              <div className="space-y-2 py-6">
-                { navigation.map((item) => (
-                  <Link
-                    key={ item.name }
-                    href={ item.href }
-                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                  >
-                    { item.name }
-                  </Link>
-                )) }
-              </div>
-              <div className="py-6">
-                <Link
-                  href="/login"
-                  className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                >
-                  Log in
-                </Link>
-              </div>
-            </div>
-          </div>
-        </DialogPanel>
-      </Dialog>
     </header>
   )
 }
